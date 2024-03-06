1d664a2c0df40d4404d3dc4d67bb49b8624cb42d
Gyeongwon Kim <kkw9709@gmail.com>
2024-03-06 17:23:06 -0500

    added weather

--- app.js
+++ app.js
@@ -157,3 +157,121 @@ const updateTime = () => {
 }
 updateTime();
 setInterval(updateTime, 1000);
+
+// Weather Section
+let weatherEl = document.getElementById("weather")
+let weatherURL = "https://api.weather.gov/"
+
+let hourlyEl = document.getElementById('hourly');
+let dailyEl = document.getElementById('daily');
+
+const createWeatherItem = (element, hourly) => {
+    let newEl = document.createElement('div');
+    newEl.className = "weather-item";
+    let timeEl = document.createElement('p');
+    let timeBold = document.createElement('b');
+    timeEl.appendChild(timeBold);
+    let tempEl = document.createElement('p');
+    let precpEl = document.createElement('p');
+    let imageEl = document.createElement('i');
+
+    timeEl.className = "weather-line";
+    tempEl.className = "weather-line";
+    precpEl.className = "weather-line";
+
+    let desc = element.shortForecast;
+    if (desc.includes("Rain")) {
+        imageEl.className = "fas fa-umbrella";
+    } else if (desc.includes("Cloud")) {
+        imageEl.className = "fas fa-cloud";
+    } else {
+        imageEl.className = "fas fa-sun";
+    }
+    imageEl.setAttribute('title', desc);
+
+    let date = new Date(element.startTime)
+    if (hourly) {
+        timeBold.innerHTML = date.getHours() > 12 ? (date.getHours() - 12).toString() + "pm" : date.getHours() + "am";
+    } else {
+        timeBold.innerHTML = date.getMonth() + "/" + date.getDate() + (date.getHours() < 18 ? " M" : " A");
+    }
+    tempEl.innerHTML = element.temperature + ' F';
+    precpEl.innerHTML = element.probabilityOfPrecipitation.value == null ? "0%" : element.probabilityOfPrecipitation.value + '%';
+
+    newEl.appendChild(timeEl);
+    newEl.appendChild(imageEl);
+    newEl.appendChild(tempEl);
+    newEl.appendChild(precpEl);
+
+    return newEl;
+}
+
+const displayWeatherHourly = (periods) => {
+    let curTime = new Date();
+    let start = 0;
+    let ptime = new Date(periods[0].startTime);
+    while (curTime > ptime) {
+        start += 1;
+        ptime = new Date(periods[start].startTime);
+    }
+    for (let index = start - 1; index < start + 23; index++) {
+        const element = periods[index];
+
+        let newEl = createWeatherItem(element, true);
+        hourlyEl.appendChild(newEl);
+    }
+}
+
+const displayWeatherDaily = (periods) => {
+    let curTime = new Date();
+    let start = 0;
+    let ptime = new Date(periods[0].startTime);
+    while (curTime > ptime) {
+        start += 1;
+        ptime = new Date(periods[start].startTime);
+    }
+    for (let index = start - 1; index < start + 8; index++) {
+        const element = periods[index];
+
+        let newEl = createWeatherItem(element, false);
+        dailyEl.appendChild(newEl);
+    } 
+}
+
+const fetchWeatherData = (latitude, longitude) => {
+    fetch(weatherURL + "points/" + latitude.toString() + "," + longitude.toString())
+    .then((response) => response.json())
+    .then((json) => {
+        fetch(json.properties.forecastHourly)
+            .then((response) => response.json())
+            .then((json) => {
+                localStorage.weatherHourly = JSON.stringify(json.properties.periods);
+            });
+        fetch(json.properties.forecast)
+            .then((response) => response.json())
+            .then((json) => {
+                localStorage.weatherDaily = JSON.stringify(json.properties.periods);
+            });
+    });
+    localStorage.weatherUpdate = Date.now();
+}
+
+const refreshWeather = () => {
+    console.log("Refreshing Weather");
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            fetchWeatherData(position.coords.latitude, position.coords.longitude);
+        });
+    } else {
+        weatherEl.innerHTML = "Cannot find current location"
+    }
+}
+
+if (localStorage.weatherUpdate != undefined && Number(localStorage.weatherUpdate) + 1000 * 60 * 60 * 24 > Date.now()) {
+    displayWeatherHourly(JSON.parse(localStorage.weatherHourly));
+    displayWeatherDaily(JSON.parse(localStorage.weatherDaily));
+} else {
+    refreshWeather();
+    displayWeatherHourly(JSON.parse(localStorage.weatherHourly));
+    displayWeatherDaily(JSON.parse(localStorage.weatherDaily));
+}
